4c5f6e5342ed395d99d7953bf820e3f167486436
Mooncak3e <120215518+Mooncak3e@users.noreply.github.com>
2023-03-28 23:51:40 +0630

    Update main.py

--- main.py
+++ main.py
@@ -186,9 +186,9 @@ elif choice_s == "Webcam Hand Detection":
     RTC_CONFIGURATION = RTCConfiguration(
         {"iceServers": [
             {
-                "urls": ["turn:numb.viagenie.ca"],
-                "username": ["webrtc@live.com"],
-                "credential": ["muazkh"]
+                "urls": ["turn:192.158.29.39:3478?transport=udp"],
+                "username": ["28224511:1379330808"],
+                "credential": ["JZEOEt2V3Qb0y27GRntt2u2PAYA="]
             }
         ]
         }
